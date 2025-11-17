7b60671fe1f690c08a3a491ed7533834caf4fbc6
youseffai <altyoxox@gmail.com>
2025-11-17 20:00:18 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,5 +1,27 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+    def split(formula):
+  my_list=[]
+  form_list=list(formula)
+  element=""
+  i=-1
+  for item in form_list:
+    
+    print(i)
+    print(my_list)
+    if item.isupper():
+      element=""
+      element+=item
+      my_list.append(element)
+      i+=1
+    if item.islower():
+      element+=item
+      my_list[i]=element
+    if item.isdigit():
+      element+=str(item)
+      my_list[i]=element
+
+  
+  return my_list
 
 
 def split_at_first_digit(formula):
