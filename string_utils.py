38c599af1ca7bef969aac363ff2012539ff3bfe6
youseffai <altyoxox@gmail.com>
2025-11-21 22:37:55 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -3,7 +3,7 @@ def split_at_digit(formula):
  for i, char in enumerate(formula):
   if char.isdigit():
     first_digit_index = i
-     break
+    break
  if first_digit_index != -1:
   prefix = formula[:first_digit_index]
   number_str = formula[first_digit_index:]
